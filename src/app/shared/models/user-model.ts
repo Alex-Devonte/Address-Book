ed7bb66d6202cc7afd7eefd5aa9634a8d75f7cd0
Alex <alex95.taylor@gmail.com>
2021-05-05 23:37:16 -0400

    Add user model

--- src/app/shared/models/user-model.ts
+++ src/app/shared/models/user-model.ts
@@ -1,7 +1,3 @@
 export class User {
-  constructor(public firstName: string, public lastName: string, public email: string, private _token: string, private _tokenExpiration: Date) {}
-
-  get token() {
-    return this._token;
-  }
+  constructor(private id: string, public firstName: string, public lastName: string, public email: string) {}
 }
