e5a2afcbcf6103c62199cce5de8a9ba0980ae0a7
Alex <alex95.taylor@gmail.com>
2021-05-06 12:56:29 -0400

    Add fucntions to check the login status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { TokenStorageService } from '../token-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService ) { }
+  loggedIn = false;
 
   ngOnInit(): void {
   }
 
+  ngDoCheck() {
+    this.checkLogin();
+  }
+
+  checkLogin() {
+    if (localStorage.getItem(this.tokenStorage.tokenKey)) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
+
   logout() {
+    this.loggedIn = false;
     this.authService.logout();
   }
 
