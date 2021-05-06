758bacedf633135d2944cbc8900cd2b86c99a482
Alex-Devonte <alex95.taylor@gmail.com>
2021-05-05 21:19:59 -0400

    Delete src/app/shared/models directory

--- src/app/shared/models/user-model.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-  constructor(public firstName: string, public lastName: string, public email: string, private _token: string, private _tokenExpiration: Date) {}
-
-  get token() {
-    return this._token;
-  }
-}
