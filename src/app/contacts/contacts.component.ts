c3d460192b242c09b385bfc11a735a9afdaedcb6
Alex <alex95.taylor@gmail.com>
2021-05-17 22:49:13 -0400

    Add logic for displaying add contact form

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -11,7 +11,7 @@ export class ContactsComponent implements OnInit {
 
   contactList = [{firstName: "", lastName: ""}];
   userId = this.tokenStorage.getToken().id;
-
+  displayContactForm = false;
   numberOfContacts = 0;
 
   constructor(private contactService: ContactsService, private tokenStorage: TokenStorageService) {}
@@ -33,6 +33,8 @@ export class ContactsComponent implements OnInit {
     });
   }
 
-  
+  addContact() {
+    this.displayContactForm = !this.displayContactForm;
+  }
 
 }
