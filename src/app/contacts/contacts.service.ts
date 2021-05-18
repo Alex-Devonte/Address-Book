fc31833bdfce1b605f630435a2c267beaec608a1
Alex <alex95.taylor@gmail.com>
2021-05-17 22:42:11 -0400

    Add remaining CRUD function calls

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -13,7 +13,17 @@ export class ContactsService {
     return this.http.post(environment.getContactsUrl, {id: id});
   }
 
-  getContact(userId: string, contactId: string) {
+  addContact(id: string) {
+    return this.http.post(environment.addContactUrl, {id: id});
+  }
 
+  editContact(userId: string, contactId: string) {
+    return this.http.put(environment.editContactUrl, {userId: userId, contactId: contactId});
+  }
+
+  deleteContact(userId: string, contactId: string) {
+    let httpParams = new HttpParams().set(userId, userId);
+    httpParams.set(contactId, contactId);
+    return this.http.delete(environment.deleteContactUrl, {params: httpParams});
   }
 }
