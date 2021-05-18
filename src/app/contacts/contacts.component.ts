9944f43779d21606e5a96ce2934a76b7ac60d0db
Alex <alex95.taylor@gmail.com>
2021-05-17 22:47:37 -0400

    Add logic for getting number of contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -12,6 +12,8 @@ export class ContactsComponent implements OnInit {
   contactList = [{firstName: "", lastName: ""}];
   userId = this.tokenStorage.getToken().id;
 
+  numberOfContacts = 0;
+
   constructor(private contactService: ContactsService, private tokenStorage: TokenStorageService) {}
 
   ngOnInit(): void {
@@ -22,14 +24,15 @@ export class ContactsComponent implements OnInit {
   getContactList() {
     this.contactService.getContacts(this.userId).subscribe(res => {
       let resData = Object.values(res);
-
       for (var i = 0; i < resData.length; i++) {
         let firstName = resData[i].first_name;
         let lastName = resData[i].last_name;
-
+        this.numberOfContacts = resData.length;
         this.contactList.push ({firstName: firstName, lastName: lastName});
       }
     });
   }
+
+  
 
 }
