eb4655b4c98a35cd07461ad2e5c55376202e7a84
Alex <alex95.taylor@gmail.com>
2021-05-19 19:41:27 -0400

    Add form submit logic

--- src/app/contacts/contact-add/contact-add.component.ts
+++ src/app/contacts/contact-add/contact-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TokenStorageService } from 'src/app/token-storage.service';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-add',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-add.component.scss']
 })
 export class ContactAddComponent implements OnInit {
-
-  constructor() { }
+  imageSrc: any;
+  constructor(private contactService: ContactsService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+  }
+
+  showPreview(event: any) {
+    console.log(event.target.files);
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result;
+
+      reader.readAsDataURL(file);
+  }
+}
+
+  onSubmit(form: NgForm) {
+    const contactData = form.value;
+    this.contactService.addContact(this.tokenStorage.getToken().id, contactData).subscribe(res => {
+      console.log(res);
+    });
   }
 
 }
