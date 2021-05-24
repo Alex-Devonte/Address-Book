2dc7c5ef4b15714fdc9ff7ba4187ee3755621346
Alex <alex95.taylor@gmail.com>
2021-05-23 20:31:32 -0400

    Refactor: update query to use inner joins
    The select query is now setup to pull data from all three tables

--- src/php/getContacts.php
+++ src/php/getContacts.php
@@ -4,17 +4,27 @@
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Headers: Content-Type");
 
-  //Get raw data
-  $data = json_decode(file_get_contents("php://input"));
-  $data = (array_values((array)$data));
+  $data = (file_get_contents("php://input"));
 
-  $selectQuery = "SELECT * FROM contacts WHERE user_id = :id";
-  $statement = $connection->prepare($selectQuery);
-  $statement->execute(array(
-    ":id" => $data[0]
-  ));
+  try
+  {
+    $selectQuery = "SELECT * FROM contacts 
+                    INNER JOIN email_addresses 
+                    ON contacts.contact_id = email_addresses.contact_id
+                    INNER JOIN phone_numbers
+                    ON contacts.contact_id = phone_numbers.contact_id
+                    WHERE user_id = :id";
 
-  $row = $statement->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($row);
+    $statement = $connection->prepare($selectQuery);
+    $statement->execute(array(
+      ":id" => $data
+    ));
 
+    $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($row);
+  }
+  catch (PDOException $e) {
+    echo $e;
+  }
 ?>
