73ecfe0960c3d576adfa5b6ab00f6f1b4dee51be
Alex <alex95.taylor@gmail.com>
2021-05-29 20:46:55 -0400

    Refactor: Add logic utilizing the Contact model

--- src/app/dashboard/contacts/contacts.component.ts
+++ src/app/dashboard/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
+import { Contact } from 'src/app/shared/models/contact-model';
 import { TokenStorageService } from '../../token-storage.service';
 import { ContactsService } from './contacts.service';
 
@@ -9,15 +10,16 @@ import { ContactsService } from './contacts.service';
 })
 export class ContactsComponent implements OnInit, DoCheck {
 
-  contactList = [{firstName: "", lastName: ""}];
+  contactList = [new Contact("","","","","","","","","","")];
+
   userId = this.tokenStorage.getToken().id;
   numberOfContacts = 0;
 
   constructor(private contactService: ContactsService, private tokenStorage: TokenStorageService) {}
 
   ngOnInit(): void {
-    this.contactList.pop();
     this.getContactList();
+    
   }
 
   ngDoCheck() {
@@ -33,7 +35,6 @@ export class ContactsComponent implements OnInit, DoCheck {
 
   getContactList() {
     this.contactService.getContacts(this.userId).subscribe(res => {
-      console.log(res);
       //Allows access to the properties tied to an Object
       let resData = Object.values(res);
 
@@ -41,10 +42,18 @@ export class ContactsComponent implements OnInit, DoCheck {
       this.contactList.splice(0, this.contactList.length);
       
       for (var i = 0; i < resData.length; i++) {
+        let id = resData[i].contact_id;
         let firstName = resData[i].first_name;
         let lastName = resData[i].last_name;
+        let nickname = resData[i].nickname;
+        let email = resData[i].email;
+        let emailType = resData[i].email_type;
+        let phone = resData[i].phone;
+        let phoneType = resData[i].phoneType;
+        let picPath = resData[i].profile_picture_path; 
+        let userId = resData[i].user_id;
         this.numberOfContacts = resData.length;
-        this.contactList.push ({firstName: firstName, lastName: lastName});
+        this.contactList.push(new Contact(id, firstName, lastName, nickname, email, emailType, phone, phoneType, picPath, userId));
       }
     });
   }
