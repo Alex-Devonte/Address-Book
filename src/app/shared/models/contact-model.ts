a85d9fd75ace809e21c0dafc3ceeb453bfcf9611
Alex <alex95.taylor@gmail.com>
2021-05-29 20:44:50 -0400

    Feat: Add Contact model

---
+++ src/app/shared/models/contact-model.ts
@@ -0,0 +1,5 @@
+export class Contact {
+  constructor(public id: string, public firstName: string, public lastName: string,
+              public nickname: string,public email: string,
+              public emailType: string, public phone: string, public phoneType: string, public path: string, public uid: string) {}
+}
