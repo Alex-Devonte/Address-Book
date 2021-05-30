d0106963b513dd94e27cd3676267361999796181
Alex <alex95.taylor@gmail.com>
2021-05-30 18:10:17 -0400

    Feat: Add function to get contact information

--- src/app/dashboard/contacts/contact/contact.component.ts
+++ src/app/dashboard/contacts/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Contact } from 'src/app/shared/models/contact-model';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contactService: ContactsService) { }
+  contact = [new Contact("","","","","","","","","","")];
 
   ngOnInit(): void {
+
   }
 
+  getContact(id: any) {
+    this.contactService.getContact(id).subscribe(res => {
+     this.contact = [new Contact(res[0].contact_id, res[0].first_name, res[0].last_name,
+                                res[0].nickname, res[0].email_address, res[0].email_type,
+                                res[0].phone_number, res[0].phone_type, res[0].profile_picture_path,
+                                res[0].user_id)];
+    });
+  }
 }
