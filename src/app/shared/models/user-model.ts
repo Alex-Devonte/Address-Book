4794f4a197214d5beef000f89e6720735cb810bb
Alex <alex95.taylor@gmail.com>
2021-05-31 20:22:01 -0400

    Change private modifier to public

--- src/app/shared/models/user-model.ts
+++ src/app/shared/models/user-model.ts
@@ -1,3 +1,3 @@
 export class User {
-  constructor(private id: string, public firstName: string, public lastName: string, public email: string) {}
+  constructor(public id: string, public firstName: string, public lastName: string, public email: string) {}
 }
