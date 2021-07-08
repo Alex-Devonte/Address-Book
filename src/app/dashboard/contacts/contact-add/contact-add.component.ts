306e186985cdd3a1b7fe2da234ba88b63c596adc
Alex <alex95.taylor@gmail.com>
2021-07-08 17:45:03 -0400

    Add ng2 file uploader

--- src/app/dashboard/contacts/contact-add/contact-add.component.ts
+++ src/app/dashboard/contacts/contact-add/contact-add.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TokenStorageService } from 'src/app/token-storage.service';
 import { ContactsService } from '../../contacts/contacts.service';
+import { FileUploader, FileLikeObject, FileItem } from 'ng2-file-upload';
+
 
 @Component({
   selector: 'app-contact-add',
@@ -11,6 +13,9 @@ import { ContactsService } from '../../contacts/contacts.service';
 })
 export class ContactAddComponent implements OnInit {
   imageSrc: any;
+  url = "http://localhost/address-book/src/php/addContact.php";
+  myFile: any;
+
   constructor(private router: Router, private contactService: ContactsService, private tokenStorage: TokenStorageService) { }
 
   emails = [{
@@ -25,7 +30,19 @@ export class ContactAddComponent implements OnInit {
     phoneType: ''     
   }];
 
+  //ng2-File-Upload
+  public uploader: FileUploader = new FileUploader({
+    url: this.url,
+    disableMultipart: false,
+    autoUpload: true,
+    itemAlias: 'attachment'
+  });
+
+
   ngOnInit(): void {
+    this.uploader.onBeforeUploadItem = (item) => {
+      item.withCredentials = false;
+    }
   }
 
   //Access FileList form the File API to show a preview of the user's uploaded image
@@ -39,6 +56,11 @@ export class ContactAddComponent implements OnInit {
       reader.readAsDataURL(file);
     }
   } 
+
+  onFileSelected(event: File[]) {
+   const file: File = event[0];
+   this.myFile = file;
+  }
 
   addEmailField() {
     this.emails.push({
@@ -60,6 +82,11 @@ export class ContactAddComponent implements OnInit {
     const contactData = form.value;
     contactData.emailGroup = this.emails;
     contactData.phoneGroup = this.phones;
+
+    //Only set the picture location if one was uploaded
+    if (this.uploader.isFile(this.myFile)) {
+      contactData.profilePic = "http://localhost/address-book/src/php/images/" + this.myFile.name;
+    }
 
     this.contactService.addContact(this.tokenStorage.getToken().id, contactData).subscribe(res => {
       this.contactService.refreshContactList();
